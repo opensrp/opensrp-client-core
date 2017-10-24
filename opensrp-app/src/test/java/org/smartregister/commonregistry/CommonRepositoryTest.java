f1dfe9bb12898e752edd9d7fb8e5556dda7f93c3
raihan-mpower <raihan@mpower-social.com>
2017-10-24 20:44:56 +0600

    RA| added test for add in common repository

--- opensrp-app/src/test/java/org/smartregister/commonregistry/CommonRepositoryTest.java
+++ opensrp-app/src/test/java/org/smartregister/commonregistry/CommonRepositoryTest.java
@@ -8,41 +8,63 @@ import junit.framework.Assert;
 import net.sqlcipher.Cursor;
 import net.sqlcipher.database.SQLiteDatabase;
 
+import org.fest.assertions.api.ANDROID;
 import org.junit.Before;
+import org.junit.ClassRule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
+import org.powermock.api.support.membermodification.MemberModifier;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.rule.PowerMockRule;
 import org.smartregister.BaseUnitTest;
 import org.smartregister.Context;
+import org.smartregister.commonregistry.shared.FakeRepository;
+import org.smartregister.repository.DrishtiRepository;
 import org.smartregister.repository.Repository;
 import org.smartregister.service.AlertService;
+import org.smartregister.util.Session;
+import org.smartregister.view.activity.DrishtiApplication;
 
+import java.io.File;
+import java.lang.reflect.Field;
 import java.util.HashMap;
 
 import static junit.framework.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
 /**
  * Created by onaio on 29/08/2017.
  */
-
+@PrepareForTest({DrishtiApplication.class})
 public class CommonRepositoryTest extends BaseUnitTest {
+
+    public static final String ADDITIONALCOLUMN = "ADDITIONALCOLUMN";
+    public static final String CUSTOMRELATIONALID = "CUSTOMRELATIONALID";
 
     @InjectMocks
     private CommonRepository commonRepository;
 
     @InjectMocks
     private Repository repository;
+
+    @InjectMocks
+    private FakeRepository fakerepository;
 
     @Mock
     private CommonFtsObject commonFtsObject;
@@ -71,6 +93,15 @@ public class CommonRepositoryTest extends BaseUnitTest {
         commonFtsObject = Mockito.mock(CommonFtsObject.class);
         CommonRepository commonRepository = new CommonRepository(tablename,tableColumns);
         assertNotNull(commonRepository);
+        Assert.assertNotNull(new CommonRepository(commonFtsObject,tablename,tableColumns));
+    }
+
+   @Test
+    public void instantiatesSuccessfullyOnConstructorCallWithAdditionalColumns() throws Exception {
+        String tablename = "";
+        String [] tableColumns = new String[] {CommonRepository.Relational_Underscore_ID, CommonRepository.BASE_ENTITY_ID_COLUMN, ADDITIONALCOLUMN, CUSTOMRELATIONALID};
+        commonFtsObject = Mockito.mock(CommonFtsObject.class);
+        when(commonFtsObject.getCustomRelationalId(anyString())).thenReturn(CUSTOMRELATIONALID);
         Assert.assertNotNull(new CommonRepository(commonFtsObject,tablename,tableColumns));
     }
 
