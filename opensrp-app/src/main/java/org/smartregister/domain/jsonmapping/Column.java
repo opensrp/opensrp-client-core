9a807899bb01f87b8311748004c5d12589bf2d28
rkodev <43806892+rkodev@users.noreply.github.com>
2019-07-18 10:11:35 +0300

    Added serialized object names

--- opensrp-app/src/main/java/org/smartregister/domain/jsonmapping/Column.java
+++ opensrp-app/src/main/java/org/smartregister/domain/jsonmapping/Column.java
@@ -1,5 +1,7 @@
 package org.smartregister.domain.jsonmapping;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by keyman on 2/21/2018.
  */
@@ -7,8 +9,11 @@ package org.smartregister.domain.jsonmapping;
 public class Column {
     public String column_name;
     public String type;
+    @SerializedName("data_type")
     public String dataType;
+    @SerializedName("source_format")
     public String sourceFormat;
+    @SerializedName("save_format")
     public String saveFormat;
     public JsonMapping json_mapping;
 }
