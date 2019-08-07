0cc35f1a01518d1489e3d7722839c55de89d321a
Samuel Githengi <samuel.githengi@gmail.com>
2019-08-07 18:06:14 +0300

    Sync Task service job allow override sync service

--- opensrp-app/src/main/java/org/smartregister/job/SyncTaskServiceJob.java
+++ opensrp-app/src/main/java/org/smartregister/job/SyncTaskServiceJob.java
@@ -4,16 +4,24 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 
 import org.smartregister.AllConstants;
+import org.smartregister.sync.intent.SyncIntentService;
 import org.smartregister.sync.intent.SyncTaskIntentService;
 
 public class SyncTaskServiceJob extends BaseJob {
 
     public static final String TAG = "SyncTaskServiceJob";
 
+
+    private Class<? extends SyncTaskIntentService> serviceClass;
+
+    public SyncTaskServiceJob(Class<? extends SyncTaskIntentService> serviceClass) {
+        this.serviceClass = serviceClass;
+    }
+
     @NonNull
     @Override
     protected Result onRunJob(@NonNull Params params) {
-        Intent intent = new Intent(getApplicationContext(), SyncTaskIntentService.class);
+        Intent intent = new Intent(getApplicationContext(), serviceClass);
         getApplicationContext().startService(intent);
         return params != null && params.getExtras().getBoolean(AllConstants.INTENT_KEY.TO_RESCHEDULE, false) ? Result.RESCHEDULE : Result.SUCCESS;
     }
