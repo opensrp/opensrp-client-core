df49be257df79950e4bca01e2c5fe2d3ca65fcd4
richard <rkareko@ona.io>
2020-05-05 11:55:49 +0300

    Added filter by column test

--- opensrp-app/src/test/java/org/smartregister/commonregistry/CommonObjectFilterOptionTest.java
+++ opensrp-app/src/test/java/org/smartregister/commonregistry/CommonObjectFilterOptionTest.java
@@ -15,6 +15,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.smartregister.commonregistry.CommonObjectFilterOption.ByColumnAndByDetails.byColumn;
+import static org.smartregister.commonregistry.CommonObjectFilterOption.ByColumnAndByDetails.byDetails;
 
 /**
  * Created by Richard Kareko on 5/5/20.
@@ -28,13 +29,13 @@ public class CommonObjectFilterOptionTest extends BaseUnitTest {
     private String fieldname;
     private String criteria;
     private String filterOptionName;
-    private Map<String, String> emptyDetails;
+    private Map<String, String> emptyMap;
 
     private CommonObjectFilterOption commonObjectFilterOption;
 
     @Before
     public void setUp() {
-        emptyDetails = Collections.emptyMap();
+        emptyMap = Collections.emptyMap();
         fieldname = "name";
         criteria = "Woman A";
         filterOptionName = "gender";
@@ -54,11 +55,28 @@ public class CommonObjectFilterOptionTest extends BaseUnitTest {
     @Test
     public void testFilterByColumn() {
         Map<String, String> column1 = EasyMap.create("name", "Woman A").map();
-        CommonPersonObjectClient expectedClient1 = new CommonPersonObjectClient("entity id 1",
-                emptyDetails,
+        CommonPersonObjectClient expectedClient = new CommonPersonObjectClient("entity id 1",
+                emptyMap,
                 "Woman A");
-        expectedClient1.setColumnmaps(column1);
-        boolean filter = commonObjectFilterOption.filter(expectedClient1);
+        expectedClient.setColumnmaps(column1);
+        boolean filter = commonObjectFilterOption.filter(expectedClient);
+        assertTrue(filter);
+    }
+
+    @Test
+    public void testFilterOptionName() {
+        assertEquals(filterOptionName, commonObjectFilterOption.name());
+    }
+
+    @Test
+    public void testFilterByDetails() {
+        commonObjectFilterOption = new CommonObjectFilterOption(criteria, fieldname, byDetails, filterOptionName);
+        Map<String, String> detail = EasyMap.create("name", "Woman A").map();
+        CommonPersonObjectClient expectedClient = new CommonPersonObjectClient("entity id 1",
+                detail,
+                "Woman A");
+        expectedClient.setColumnmaps(emptyMap);
+        boolean filter = commonObjectFilterOption.filter(expectedClient);
         assertTrue(filter);
     }
 
