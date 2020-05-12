9f2d4ac118f2fa08d570068731ab552ef2cf05b4
richard <rkareko@ona.io>
2020-05-12 11:13:54 +0300

    Added unit tests

--- opensrp-app/src/test/java/org/smartregister/adapter/SmartRegisterQueryBuilderTest.java
+++ opensrp-app/src/test/java/org/smartregister/adapter/SmartRegisterQueryBuilderTest.java
@@ -35,4 +35,36 @@ public class SmartRegisterQueryBuilderTest extends BaseUnitTest {
         assertEquals(expectedQuery, Whitebox.getInternalState(smartRegisterQueryBuilder, "Selectquery"));
     }
 
+    @Test
+    public void testQueryForCountOnRegisters() {
+        String expectedTable = "task";
+        String expectedCondition = "status IS NULL";
+
+        String actualQuery = smartRegisterQueryBuilder.queryForCountOnRegisters(expectedTable, expectedCondition);
+        assertEquals("SELECT COUNT (*)  FROM task WHERE status IS NULL", actualQuery);
+    }
+
+    @Test
+    public void testQueryForCountOnRegistersWithoutCondition() {
+        String expectedTable = "task";
+
+        String actualQuery = smartRegisterQueryBuilder.queryForCountOnRegisters(expectedTable, null);
+        assertEquals("SELECT COUNT (*)  FROM task", actualQuery);
+    }
+
+    @Test
+    public void testSelectInitiateMainTable() {
+        String[] tableNames = {"event"};
+        String[] columns = {"createdAt", "baseEntityId"};
+
+        String actualQuery = smartRegisterQueryBuilder.selectInitiateMainTable(tableNames, columns);
+        assertEquals("Select event.id as _id , createdAt , baseEntityId From event", actualQuery);
+    }
+
+    @Test
+    public void testSelectInitiateMainTableCounts() {
+        String actualQuery = smartRegisterQueryBuilder.selectInitiateMainTableCounts("event");
+        assertEquals("SELECT COUNT(*) FROM event", actualQuery);
+    }
+
 }
