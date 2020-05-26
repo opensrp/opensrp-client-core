3d098bc313ec8532d0b16d3dc67f2cf965fabee7
richard <rkareko@ona.io>
2020-05-26 10:33:35 +0300

    Added AllCommonsRepository tests

--- opensrp-app/src/test/java/org/smartregister/commonregistry/AllCommonsRepositoryTest.java
+++ opensrp-app/src/test/java/org/smartregister/commonregistry/AllCommonsRepositoryTest.java
@@ -1,10 +1,8 @@
 package org.smartregister.commonregistry;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.smartregister.BaseUnitTest;
 import org.smartregister.repository.AlertRepository;
@@ -13,6 +11,10 @@ import org.smartregister.repository.TimelineEventRepository;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /*
  by Raihan Ahmed
@@ -43,29 +45,39 @@ public class AllCommonsRepositoryTest extends BaseUnitTest {
                 "relationid1",
                 new HashMap<String, String>(),
                 ""));
-        Mockito.when(personRepository.allcommon()).thenReturn(expectedpersonobjects);
-        Assert.assertEquals(expectedpersonobjects, personRepository.allcommon());
+        when(personRepository.allcommon()).thenReturn(expectedpersonobjects);
+        assertEquals(expectedpersonobjects, personRepository.allcommon());
     }
 
     @Test
     public void ShouldShowcount() throws Exception {
-        Mockito.when(personRepository.count()).thenReturn((long) 8);
-        Assert.assertEquals(personRepository.count(), (long) 8);
+        when(personRepository.count()).thenReturn((long) 8);
+        assertEquals(personRepository.count(), (long) 8);
 
     }
-//    public List<CommonPersonObject> findByCaseIDs(List<String> caseIds) {
-//        return personRepository.findByCaseIDs(caseIds.toArray(new String[caseIds.size()]));
-//    }
-//
-//
-//
-//    public void close(String entityId) {
-//        alertRepository.deleteAllAlertsForEntity(entityId);
-//        timelineEventRepository.deleteAllTimelineEventsForEntity(entityId);
-//        personRepository.close(entityId);
-//    }
-//
-//    public void mergeDetails(String entityId, Map<String, String> details) {
-//        personRepository.mergeDetails(entityId, details);
-//    }
+
+    @Test
+    public void testAll() {
+        allCommonsRepository.all();
+        verify(personRepository).allcommon();
+    }
+
+    @Test
+    public void testFindByCaseId() {
+        allCommonsRepository.findByCaseID("case 1");
+        verify(personRepository).findByCaseID("case 1");
+    }
+
+    @Test
+    public void testFindHHByGOBHHID() {
+        allCommonsRepository.findHHByGOBHHID("gobhhid 1");
+        verify(personRepository).findHHByGOBHHID("gobhhid 1");
+    }
+
+    @Test
+    public void testCount() {
+        allCommonsRepository.count();
+        verify(personRepository).count();
+    }
+
 }
