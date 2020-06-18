c80edde1bbffecdc11f617392dd863997a7e60f3
richard <rkareko@ona.io>
2020-06-18 15:28:29 +0300

    Fix build error after merge

--- opensrp-app/src/main/java/org/smartregister/sync/helper/LocationServiceHelper.java
+++ opensrp-app/src/main/java/org/smartregister/sync/helper/LocationServiceHelper.java
@@ -281,6 +281,8 @@ public class LocationServiceHelper {
                     if (!unprocessedIds.contains(location.getId()))
                         locationRepository.markLocationsAsSynced(location.getId());
                 }
+            }
+        }
     }
     public void fetchOpenMrsLocationsByTeamIds() throws NoHttpResponseException, JSONException {
         HTTPAgent httpAgent = getHttpAgent();
