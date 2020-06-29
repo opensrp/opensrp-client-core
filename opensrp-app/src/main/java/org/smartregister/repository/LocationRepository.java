86c944b1aab27b5e3f169e94b9037010f9b39c9f
Samuel Githengi <samuel.githengi@gmail.com>
2020-06-29 21:43:23 +0300

    Implement location and structures data interfaces

--- opensrp-app/src/main/java/org/smartregister/repository/LocationRepository.java
+++ opensrp-app/src/main/java/org/smartregister/repository/LocationRepository.java
@@ -9,13 +9,17 @@ import net.sqlcipher.Cursor;
 import net.sqlcipher.database.SQLiteDatabase;
 
 import org.apache.commons.lang3.StringUtils;
+import org.smartregister.converters.LocationConverter;
 import org.smartregister.domain.Location;
 import org.smartregister.domain.LocationProperty;
+import org.smartregister.domain.PhysicalLocation;
 import org.smartregister.pathevaluator.dao.LocationDao;
 import org.smartregister.util.PropertiesConverter;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import timber.log.Timber;
 
@@ -36,7 +40,7 @@ public class LocationRepository extends BaseRepository implements LocationDao {
 
     protected static final String LOCATION_TABLE = "location";
 
-    protected static final String[] COLUMNS = new String[]{ID, UUID, PARENT_ID, NAME,GEOJSON};
+    protected static final String[] COLUMNS = new String[]{ID, UUID, PARENT_ID, NAME, GEOJSON};
 
     private static final String CREATE_LOCATION_TABLE =
             "CREATE TABLE " + LOCATION_TABLE + " (" +
@@ -68,7 +72,7 @@ public class LocationRepository extends BaseRepository implements LocationDao {
         contentValues.put(PARENT_ID, location.getProperties().getParentId());
         contentValues.put(NAME, location.getProperties().getName());
         contentValues.put(GEOJSON, gson.toJson(location));
-        contentValues.put(SYNC_STATUS, location.getSyncStatus() );
+        contentValues.put(SYNC_STATUS, location.getSyncStatus());
         getWritableDatabase().replace(getLocationTableName(), null, contentValues);
 
     }
@@ -110,9 +114,13 @@ public class LocationRepository extends BaseRepository implements LocationDao {
     }
 
     public Location getLocationById(String id) {
+        return getLocationById(id, getLocationTableName());
+    }
+
+    public Location getLocationById(String id, String tableName) {
         Cursor cursor = null;
         try {
-            cursor = getReadableDatabase().rawQuery("SELECT * FROM " + getLocationTableName() +
+            cursor = getReadableDatabase().rawQuery("SELECT * FROM " + tableName +
                     " WHERE " + ID + " =?", new String[]{id});
             if (cursor.moveToFirst()) {
                 return readCursor(cursor);
@@ -126,6 +134,7 @@ public class LocationRepository extends BaseRepository implements LocationDao {
         return null;
 
     }
+
 
     public Location getLocationByUUId(String uuid) {
         Cursor cursor = null;
@@ -145,11 +154,16 @@ public class LocationRepository extends BaseRepository implements LocationDao {
 
     }
 
+
     public List<Location> getLocationsByParentId(String parentId) {
+        return getLocationsByParentId(parentId, getLocationTableName());
+    }
+
+    public List<Location> getLocationsByParentId(String parentId, String tableName) {
         Cursor cursor = null;
         List<Location> locations = new ArrayList<>();
         try {
-            cursor = getReadableDatabase().rawQuery("SELECT * FROM " + getLocationTableName() +
+            cursor = getReadableDatabase().rawQuery("SELECT * FROM " + tableName +
                     " WHERE " + PARENT_ID + " =?", new String[]{parentId});
             while (cursor.moveToNext()) {
                 locations.add(readCursor(cursor));
@@ -197,7 +211,7 @@ public class LocationRepository extends BaseRepository implements LocationDao {
      * Get a List of locations that either match or don't match the list of ids provided
      * depending on value of the inclusive flag
      *
-     * @param ids list of location ids
+     * @param ids       list of location ids
      * @param inclusive flag that determines whether the list of locations returned
      *                  should include the locations whose ids match the params provided
      *                  or exclude them
@@ -266,19 +280,21 @@ public class LocationRepository extends BaseRepository implements LocationDao {
 
     @Override
     public List<com.ibm.fhir.model.resource.Location> findJurisdictionsById(String id) {
-        //TODO implement method
-        return null;
+        PhysicalLocation location = getLocationById(id);
+        return Collections.singletonList(LocationConverter.convertPhysicalLocationToLocationResource(location));
     }
 
     @Override
     public List<com.ibm.fhir.model.resource.Location> findLocationsById(String id) {
-        //TODO implement method
-        return null;
+        PhysicalLocation location = getLocationById(id,StructureRepository.STRUCTURE_TABLE);
+        return Collections.singletonList(LocationConverter.convertPhysicalLocationToLocationResource(location));
     }
 
     @Override
     public List<com.ibm.fhir.model.resource.Location> findLocationByJurisdiction(String jurisdiction) {
-        //TODO implement method
-        return null;
+        return getLocationsByParentId(jurisdiction, StructureRepository.STRUCTURE_TABLE)
+                .stream()
+                .map(LocationConverter::convertPhysicalLocationToLocationResource)
+                .collect(Collectors.toList());
     }
 }
