f9d96f2973d35c8ae3f324096a3263bcd5f4d7ed
Samuel Githengi <samuel.githengi@gmail.com>
2020-07-07 10:53:23 +0300

    unit test activity result

--- opensrp-app/src/test/java/org/smartregister/view/activity/BaseRegisterActivityTest.java
+++ opensrp-app/src/test/java/org/smartregister/view/activity/BaseRegisterActivityTest.java
@@ -1,11 +1,15 @@
 package org.smartregister.view.activity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.SnackbarContentLayout;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.vision.barcode.Barcode;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -15,6 +19,8 @@ import org.robolectric.Robolectric;
 import org.robolectric.android.controller.ActivityController;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowToast;
+import org.smartregister.AllConstants;
+import org.smartregister.AllConstants.BARCODE;
 import org.smartregister.BaseRobolectricUnitTest;
 import org.smartregister.CoreLibrary;
 import org.smartregister.R;
@@ -26,11 +32,16 @@ import org.smartregister.view.contract.BaseRegisterContract;
 import org.smartregister.view.fragment.BaseRegisterFragment;
 import org.smartregister.view.viewpager.OpenSRPViewPager;
 
+import java.util.UUID;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
@@ -54,6 +65,9 @@ public class BaseRegisterActivityTest extends BaseRobolectricUnitTest {
 
     @Mock
     private OpenSRPViewPager mPager;
+
+    @Mock
+    private Barcode barcode;
 
     @Before
     public void setUp() {
@@ -128,7 +142,7 @@ public class BaseRegisterActivityTest extends BaseRobolectricUnitTest {
         Snackbar.SnackbarLayout layout = (Snackbar.SnackbarLayout) snackbar.getView();
         assertEquals(((TextView) ((SnackbarContentLayout) layout.getChildAt(0)).getChildAt(0)).getText(), activity.getString(R.string.manual_sync_triggered));
         assertEquals(Snackbar.LENGTH_LONG, snackbar.getDuration());
-        assertTrue( snackbar.isShown());
+        assertTrue(snackbar.isShown());
     }
 
     @Test
@@ -150,5 +164,66 @@ public class BaseRegisterActivityTest extends BaseRobolectricUnitTest {
         activity.displayShortToast(R.string.manual_sync_triggered);
         assertEquals(Toast.LENGTH_SHORT, ShadowToast.getLatestToast().getDuration());
         assertEquals(activity.getString(R.string.manual_sync_triggered), ShadowToast.getTextOfLatestToast());
+    }
+
+    @Test
+    public void testGetDefaultOptionsProvider() {
+        assertNull(activity.getDefaultOptionsProvider());
+    }
+
+    @Test
+    public void testGetNavBarOptionsProvider() {
+        assertNull(activity.getNavBarOptionsProvider());
+    }
+
+    @Test
+    public void testClientsProvider() {
+        assertNull(activity.clientsProvider());
+    }
+
+    @Test
+    public void testSetupViewsDoesNothing() {
+        activity = spy(activity);
+        activity.setupViews();
+        verify(activity).setupViews();
+        verifyNoMoreInteractions(activity);
+    }
+
+    @Test
+    public void testOnResumption() {
+        activity.onResumption();
+        verify(activity.presenter).registerViewConfigurations(null);
+    }
+
+    @Test
+    public void testOnInitializationDoesNothing() {
+        activity = spy(activity);
+        activity.onInitialization();
+        verify(activity).onInitialization();
+        verifyNoMoreInteractions(activity);
+    }
+
+    @Test
+    public void testOnActivityResultWithBarcodeResult() {
+        Intent intent = new Intent();
+        intent.putExtra(BARCODE.BARCODE_KEY, barcode);
+        String code = UUID.randomUUID().toString();
+        Whitebox.setInternalState(barcode, "displayValue", code);
+        Whitebox.setInternalState(activity, "mBaseFragment", fragment);
+        activity.onActivityResult(BARCODE.BARCODE_REQUEST_CODE, Activity.RESULT_OK, intent);
+        verify(fragment).onQRCodeSucessfullyScanned(code);
+        verify(fragment).setSearchTerm(code);
+
+    }
+
+    @Test
+    public void testOnActivityResultInvokesOnActivityResultExtended() {
+        Whitebox.setInternalState(activity, "mBaseFragment", fragment);
+        activity=spy(activity);
+        activity.onActivityResult(AllConstants.FORM_SUCCESSFULLY_SUBMITTED_RESULT_CODE, Activity.RESULT_OK, null);
+        verify(fragment,never()).onQRCodeSucessfullyScanned(anyString());
+        verify(fragment,never()).setSearchTerm(anyString());
+        verify(activity).onActivityResultExtended(AllConstants.FORM_SUCCESSFULLY_SUBMITTED_RESULT_CODE, Activity.RESULT_OK, null);
+
     }
 }
