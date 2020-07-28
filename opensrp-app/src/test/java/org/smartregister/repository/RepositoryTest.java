e1065a9ac40225d45f785477286832a191af2355
Ephraim Kigamba <ekigamba@ona.io>
2020-07-28 11:41:50 +0300

    Add test for Repository#deleteRepository

--- opensrp-app/src/test/java/org/smartregister/repository/RepositoryTest.java
+++ opensrp-app/src/test/java/org/smartregister/repository/RepositoryTest.java
@@ -2,14 +2,17 @@ package org.smartregister.repository;
 
 import net.sqlcipher.database.SQLiteDatabase;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.rule.PowerMockRule;
+import org.robolectric.util.ReflectionHelpers;
 import org.smartregister.BaseUnitTest;
 import org.smartregister.repository.mock.RepositoryMock;
 import org.smartregister.util.Session;
@@ -39,6 +42,8 @@ public class RepositoryTest extends BaseUnitTest {
     @Mock
     private DrishtiRepository drishtiRepository;
 
+    private String dbName;
+
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
@@ -47,6 +52,11 @@ public class RepositoryTest extends BaseUnitTest {
         PowerMockito.when(drishtiApplication.getApplicationContext()).thenReturn(context);
         PowerMockito.when(context.getDir("opensrp", android.content.Context.MODE_PRIVATE)).thenReturn(new File("/"));
 
+        dbName = "drishti.db";
+
+        repository = Mockito.mock(Repository.class, Mockito.CALLS_REAL_METHODS);
+        ReflectionHelpers.setField(repository, "context", context);
+        ReflectionHelpers.setField(repository, "dbName", dbName);
     }
 
     @Test
@@ -57,6 +67,23 @@ public class RepositoryTest extends BaseUnitTest {
 //        session.setRepositoryName(AllConstants.DATABASE_NAME);
 //        DrishtiRepository drishtiRepositories[] = {DrishtiRepositoryMock.getDrishtiRepository()};
 //        Repository repository = new Repository(ContextMock.getContext(), session, drishtiRepositories);
+    }
+
+    @Test
+    public void deleteRepositoryShouldReturnTrueAndCallDeleteDatabaseAndDeleteFile() {
+
+        // Mock the file returned when getting the database path
+        File dbPath = Mockito.mock(File.class);
+        Mockito.doReturn(dbPath).when(context).getDatabasePath(dbName);
+        Mockito.doReturn(true).when(dbPath).delete();
+
+        // Run the method under test
+        Assert.assertTrue(repository.deleteRepository());
+
+        // Verify that the db is deleted & actual file is deleted
+        Mockito.verify(context).deleteDatabase(dbName);
+        Mockito.verify(context).getDatabasePath(dbName);
+        Mockito.verify(dbPath).delete();
     }
 
 }
