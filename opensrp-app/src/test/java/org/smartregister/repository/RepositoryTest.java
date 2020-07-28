4fc61312fdce1991e81f869944c508b5ac16a183
Ephraim Kigamba <ekigamba@ona.io>
2020-07-28 11:52:12 +0300

    Add test for Repository
    - Add tests for getReadableDatabase
    - Add tests for getWritableDatabase

--- opensrp-app/src/test/java/org/smartregister/repository/RepositoryTest.java
+++ opensrp-app/src/test/java/org/smartregister/repository/RepositoryTest.java
@@ -12,6 +12,7 @@ import org.mockito.MockitoAnnotations;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.rule.PowerMockRule;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.util.ReflectionHelpers;
 import org.smartregister.BaseUnitTest;
 import org.smartregister.repository.mock.RepositoryMock;
@@ -43,16 +44,20 @@ public class RepositoryTest extends BaseUnitTest {
     private DrishtiRepository drishtiRepository;
 
     private String dbName;
+    private String password;
 
     @Before
     public void setUp() {
+        dbName = "drishti.db";
+        password = "Android7832!";
+
         MockitoAnnotations.initMocks(this);
         PowerMockito.mockStatic(DrishtiApplication.class);
         PowerMockito.when(DrishtiApplication.getInstance()).thenReturn(drishtiApplication);
         PowerMockito.when(drishtiApplication.getApplicationContext()).thenReturn(context);
+        Mockito.doReturn(password).when(drishtiApplication).getPassword();
         PowerMockito.when(context.getDir("opensrp", android.content.Context.MODE_PRIVATE)).thenReturn(new File("/"));
 
-        dbName = "drishti.db";
 
         repository = Mockito.mock(Repository.class, Mockito.CALLS_REAL_METHODS);
         ReflectionHelpers.setField(repository, "context", context);
@@ -68,6 +73,39 @@ public class RepositoryTest extends BaseUnitTest {
 //        DrishtiRepository drishtiRepositories[] = {DrishtiRepositoryMock.getDrishtiRepository()};
 //        Repository repository = new Repository(ContextMock.getContext(), session, drishtiRepositories);
     }
+
+    @Test
+    public void getReadableDatabaseShouldCallGetReadableDbAndPassword() {
+        Mockito.doReturn(null).when(repository).getReadableDatabase(password);
+
+        repository.getReadableDatabase();
+
+        Mockito.verify(repository).getReadableDatabase(password);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void getReadableDatabaseShouldThrowRuntimeException() {
+        Mockito.doReturn(null).when(drishtiApplication).getPassword();
+
+        repository.getReadableDatabase();
+    }
+
+    @Test
+    public void getWritableDatabaseShouldCallGetWritableDbAndPassword() {
+        Mockito.doReturn(null).when(repository).getWritableDatabase(password);
+
+        repository.getWritableDatabase();
+
+        Mockito.verify(repository).getWritableDatabase(password);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void getWritableDatabaseShouldThrowRuntimeException() {
+        Mockito.doReturn(null).when(drishtiApplication).getPassword();
+
+        repository.getWritableDatabase();
+    }
+
 
     @Test
     public void deleteRepositoryShouldReturnTrueAndCallDeleteDatabaseAndDeleteFile() {
