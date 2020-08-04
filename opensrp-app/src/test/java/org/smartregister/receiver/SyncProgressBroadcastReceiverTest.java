c2284890141ed5484010a0daf6784a00a2c6d0a0
Ephraim Kigamba <ekigamba@ona.io>
2020-08-04 11:24:35 +0300

    Add tests for SyncProgressBroadcastReceiver
    - Add tests for onReceive method

---
+++ opensrp-app/src/test/java/org/smartregister/receiver/SyncProgressBroadcastReceiverTest.java
@@ -0,0 +1,54 @@
+package org.smartregister.receiver;
+
+import android.content.Intent;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.robolectric.RuntimeEnvironment;
+import org.smartregister.AllConstants;
+import org.smartregister.BaseRobolectricUnitTest;
+import org.smartregister.domain.SyncProgress;
+
+
+/**
+ * Created by Ephraim Kigamba - nek.eam@gmail.com on 04-08-2020.
+ */
+public class SyncProgressBroadcastReceiverTest extends BaseRobolectricUnitTest {
+
+    private SyncProgressBroadcastReceiver syncProgressBroadcastReceiver;
+
+    @Mock
+    private SyncProgressBroadcastReceiver.SyncProgressListener syncProgressListener;
+
+    @Before
+    public void setUp() throws Exception {
+        syncProgressBroadcastReceiver = new SyncProgressBroadcastReceiver(syncProgressListener);
+    }
+
+    @Test
+    public void onReceiveShouldCallListenerOnSyncProgress() {
+        SyncProgress syncProgress = Mockito.mock(SyncProgress.class);
+
+        Intent intent = new Intent();
+        intent.putExtra(AllConstants.SyncProgressConstants.SYNC_PROGRESS_DATA, syncProgress);
+
+        // Call the method under test
+        syncProgressBroadcastReceiver.onReceive(RuntimeEnvironment.application, intent);
+
+        // Verify that the listener was called
+        Mockito.verify(syncProgressListener).onSyncProgress(syncProgress);
+    }
+
+    @Test
+    public void onReceiveShouldNotCallListenerOnSyncProgressWhenBundleIsNull() {
+        Intent intent = new Intent();
+
+        // Call the method under test
+        syncProgressBroadcastReceiver.onReceive(RuntimeEnvironment.application, intent);
+
+        // Verify that the listener was called
+        Mockito.verify(syncProgressListener, Mockito.never()).onSyncProgress(Mockito.nullable(SyncProgress.class));
+    }
+}
