da800f7c7bf867212829060ca8d022bf38dc5f7b
Samuel Githengi <samuel.githengi@gmail.com>
2020-08-04 12:15:54 +0300

    Unit tests all failures during remote login

--- opensrp-app/src/test/java/org/smartregister/login/BaseLoginInteractorTest.java
+++ opensrp-app/src/test/java/org/smartregister/login/BaseLoginInteractorTest.java
@@ -7,12 +7,16 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.powermock.reflect.Whitebox;
 import org.robolectric.Robolectric;
 import org.smartregister.BaseRobolectricUnitTest;
 import org.smartregister.Context;
+import org.smartregister.CoreLibrary;
 import org.smartregister.R;
+import org.smartregister.domain.LoginResponse;
 import org.smartregister.repository.AllSharedPreferences;
+import org.smartregister.service.UserService;
 import org.smartregister.shadows.LoginInteractorShadow;
 import org.smartregister.view.contract.BaseLoginContract;
 
@@ -20,6 +24,7 @@ import java.lang.ref.WeakReference;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -41,6 +46,9 @@ public class BaseLoginInteractorTest extends BaseRobolectricUnitTest {
 
     @Mock
     private Context context;
+
+    @Mock
+    private UserService userService;
 
     @Mock
     private AllSharedPreferences allSharedPreferences;
@@ -72,6 +80,7 @@ public class BaseLoginInteractorTest extends BaseRobolectricUnitTest {
         verify(allSharedPreferences).savePreference("DRISHTI_BASE_URL", activity.getString(R.string.opensrp_url));
         verify(view).enableLoginButton(true);
         verify(view).showErrorDialog(activity.getString(R.string.remote_login_base_url_missing_error));
+        verify(view, never()).goToHome(anyBoolean());
     }
 
     @Test
@@ -82,6 +91,54 @@ public class BaseLoginInteractorTest extends BaseRobolectricUnitTest {
         verify(allSharedPreferences, never()).savePreference("DRISHTI_BASE_URL", activity.getString(R.string.opensrp_url));
         verify(view, never()).enableLoginButton(true);
         verify(view).showErrorDialog(activity.getString(R.string.remote_login_generic_error));
+        verify(view, never()).goToHome(anyBoolean());
+    }
+
+
+    @Test
+    public void testLoginAttemptsRemoteLoginAndErrorsIfNoInternetConnectivity() {
+        String user = "johndoe";
+        String pass = "qwerty";
+        Whitebox.setInternalState(CoreLibrary.getInstance().context(), "userService", userService);
+        when(userService.isValidRemoteLogin(user, pass)).thenReturn(LoginResponse.NO_INTERNET_CONNECTIVITY);
+        when(allSharedPreferences.fetchBaseURL("")).thenReturn(activity.getString(R.string.opensrp_url));
+        interactor.login(new WeakReference<>(view), user, pass);
+        verify(view).hideKeyboard();
+        verify(view).enableLoginButton(false);
+        verify(view).enableLoginButton(true);
+        verify(view).showErrorDialog(activity.getString(R.string.no_internet_connectivity));
+        verify(view, never()).goToHome(anyBoolean());
+    }
+
+
+    @Test
+    public void testLoginAttemptsRemoteLoginAndErrorsIfResponseUnknown() {
+        String user = "johndoe";
+        String pass = "qwerty";
+        Whitebox.setInternalState(CoreLibrary.getInstance().context(), "userService", userService);
+        when(userService.isValidRemoteLogin(user, pass)).thenReturn(LoginResponse.UNKNOWN_RESPONSE);
+        when(allSharedPreferences.fetchBaseURL("")).thenReturn(activity.getString(R.string.opensrp_url));
+        interactor.login(new WeakReference<>(view), user, pass);
+        verify(view).hideKeyboard();
+        verify(view).enableLoginButton(false);
+        verify(view).enableLoginButton(true);
+        verify(view).showErrorDialog(activity.getString(R.string.unknown_response));
+        verify(view, never()).goToHome(anyBoolean());
+    }
+
+    @Test
+    public void testLoginAttemptsRemoteLoginAndErrorsIfUnauthorized() {
+        String user = "johndoe";
+        String pass = "qwerty";
+        Whitebox.setInternalState(CoreLibrary.getInstance().context(), "userService", userService);
+        when(userService.isValidRemoteLogin(user, pass)).thenReturn(LoginResponse.UNAUTHORIZED);
+        when(allSharedPreferences.fetchBaseURL("")).thenReturn(activity.getString(R.string.opensrp_url));
+        interactor.login(new WeakReference<>(view), user, pass);
+        verify(view).hideKeyboard();
+        verify(view).enableLoginButton(false);
+        verify(view).enableLoginButton(true);
+        verify(view).showErrorDialog(activity.getString(R.string.unauthorized));
+        verify(view, never()).goToHome(anyBoolean());
     }
 
 
