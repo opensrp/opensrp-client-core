3f16d804022000429d70c44da29851d71b938b78
rkodev <43806892+rkodev@users.noreply.github.com>
2020-09-01 11:45:38 +0300

    Codacy clean up secured  activity

--- opensrp-app/src/test/java/org/smartregister/view/activity/SecuredActivityTest.java
+++ opensrp-app/src/test/java/org/smartregister/view/activity/SecuredActivityTest.java
@@ -10,7 +10,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
-import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.gson.Gson;
 
 import org.junit.After;
