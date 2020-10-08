30cd33263fb0538a0dc803a29499088d1d3aa406
Allan O <a.onchuru@gmail.com>
2020-10-08 12:31:05 +0300

    :recycle: Add default implementations for starFormActivity

--- opensrp-app/src/main/java/org/smartregister/view/activity/BaseRegisterActivity.java
+++ opensrp-app/src/main/java/org/smartregister/view/activity/BaseRegisterActivity.java
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Looper;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
@@ -33,6 +34,7 @@ import org.smartregister.view.contract.ConfigurableRegisterActivityContract;
 import org.smartregister.view.fragment.BaseRegisterFragment;
 import org.smartregister.view.viewpager.OpenSRPViewPager;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -204,6 +206,16 @@ public abstract class BaseRegisterActivity extends SecuredNativeSmartRegisterAct
 
     @Override
     public abstract void startFormActivity(JSONObject form);
+
+    @Override
+    public void startFormActivity(String formName, String entityId, String metaData, @Nullable HashMap<String, String> injectedFieldValues, @Nullable String clientTable) {
+        //Implement Abstract Method
+    }
+
+    @Override
+    public void startFormActivity(@NonNull JSONObject jsonForm, @Nullable HashMap<String, String> parcelableData) {
+        //Implement Abstract Method
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
