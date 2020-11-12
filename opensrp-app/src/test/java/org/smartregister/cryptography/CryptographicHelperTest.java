7c822c59c2b96e40eacf3d165faa2e93bb47c469
Simon Njoroge <snjoroge@ona.io>
2020-11-12 12:52:15 +0300

    Update CryptographicHelperTest build version to M

--- opensrp-app/src/test/java/org/smartregister/cryptography/CryptographicHelperTest.java
+++ opensrp-app/src/test/java/org/smartregister/cryptography/CryptographicHelperTest.java
@@ -5,6 +5,7 @@ import android.os.Build;
 import org.apache.commons.lang3.CharEncoding;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatchers;
@@ -27,7 +28,6 @@ public class CryptographicHelperTest {
 
     public static final String SAMPLE_STRING = "I am a high security string that needs to be hidden from prying eyes";
 
-
     public static final String SAMPLE_KEY_ALIAS = "SampleKeyAlias";
 
     private CryptographicHelper cryptographicHelper;
@@ -40,10 +40,9 @@ public class CryptographicHelperTest {
 
     @Before
     public void setUp() {
-
         MockitoAnnotations.initMocks(this);
 
-        cryptographicHelper = CryptographicHelper.getInstance(RuntimeEnvironment.application);
+        cryptographicHelper = CryptographicHelper.getInstance(RuntimeEnvironment.application.getApplicationContext());
 
         Assert.assertNotNull(cryptographicHelper);
         cryptographicHelper.setMCryptography(androidMCryptography);
@@ -53,98 +52,80 @@ public class CryptographicHelperTest {
     @Test
     public void testCryptographicHelperInitsCorrectly() {
         Assert.assertNotNull(cryptographicHelper);
-
     }
 
     @Test
     public void testCryptographicHelperEncryptInvokesAndroidMEncryptMethod() throws UnsupportedEncodingException {
-
         cryptographicHelper.encrypt(SAMPLE_STRING.getBytes(CharEncoding.UTF_8), SAMPLE_KEY_ALIAS);
 
         Mockito.verify(androidMCryptography).encrypt(ArgumentMatchers.any(byte[].class), ArgumentMatchers.anyString());
-
     }
 
     @Test
-    @Config(sdk = Build.VERSION_CODES.LOLLIPOP_MR1)
+    @Config(sdk = Build.VERSION_CODES.M)
     public void testCryptographicHelperEncryptInvokesLegacyEncryptMethod() throws UnsupportedEncodingException {
         cryptographicHelper.encrypt(SAMPLE_STRING.getBytes(CharEncoding.UTF_8), SAMPLE_KEY_ALIAS);
 
         Mockito.verify(androidLegacyCryptography).encrypt(ArgumentMatchers.any(byte[].class), ArgumentMatchers.anyString());
-
     }
-
 
     @Test
     public void testCryptographicHelperDecrypteInvokesAndroidMDecryptMethod() throws UnsupportedEncodingException {
-
         cryptographicHelper.decrypt(SAMPLE_STRING.getBytes(CharEncoding.UTF_8), SAMPLE_KEY_ALIAS);
 
         Mockito.verify(androidMCryptography).decrypt(ArgumentMatchers.any(byte[].class), ArgumentMatchers.anyString());
-
     }
 
     @Test
-    @Config(sdk = Build.VERSION_CODES.LOLLIPOP_MR1)
+    @Config(sdk = Build.VERSION_CODES.M)
     public void testCryptographicHelperDecryptInvokesLegacyDecryptMethod() throws UnsupportedEncodingException {
         cryptographicHelper.decrypt(SAMPLE_STRING.getBytes(CharEncoding.UTF_8), SAMPLE_KEY_ALIAS);
 
         Mockito.verify(androidLegacyCryptography).decrypt(ArgumentMatchers.any(byte[].class), ArgumentMatchers.anyString());
-
     }
 
     @Test
     public void testCryptographicHelperGenerateKeyInvokesAndroidMGenerateKeyMethod() {
-
         cryptographicHelper.generateKey(SAMPLE_KEY_ALIAS);
 
         Mockito.verify(androidMCryptography).generateKey(SAMPLE_KEY_ALIAS);
-
     }
 
     @Test
-    @Config(sdk = Build.VERSION_CODES.LOLLIPOP_MR1)
+    @Config(sdk = Build.VERSION_CODES.M)
     public void testCryptographicHelperGenerateKeyInvokesLegacyGenerateKeyMethod() {
         cryptographicHelper.generateKey(SAMPLE_KEY_ALIAS);
 
         Mockito.verify(androidLegacyCryptography).generateKey(SAMPLE_KEY_ALIAS);
-
     }
 
     @Test
     public void testCryptographicHelperGetKeyInvokesAndroidMGetKeyMethod() {
-
         cryptographicHelper.getKey(SAMPLE_KEY_ALIAS);
 
         Mockito.verify(androidMCryptography).getKey(SAMPLE_KEY_ALIAS);
-
     }
 
     @Test
-    @Config(sdk = Build.VERSION_CODES.LOLLIPOP_MR1)
+    @Config(sdk = Build.VERSION_CODES.M)
     public void testCryptographicHelperGetKeyInvokesLegacyGetKeyMethod() {
         cryptographicHelper.getKey(SAMPLE_KEY_ALIAS);
 
         Mockito.verify(androidLegacyCryptography).getKey(SAMPLE_KEY_ALIAS);
-
     }
 
     @Test
     public void testCryptographicHelperDeleteKeyInvokesAndroidMGetKeyMethod() {
-
         cryptographicHelper.deleteKey(SAMPLE_KEY_ALIAS);
 
         Mockito.verify(androidMCryptography).deleteKey(SAMPLE_KEY_ALIAS);
-
     }
 
     @Test
-    @Config(sdk = Build.VERSION_CODES.LOLLIPOP_MR1)
+    @Config(sdk = Build.VERSION_CODES.M)
     public void testCryptographicHelperDeleteKeyInvokesLegacyGetKeyMethod() {
         cryptographicHelper.deleteKey(SAMPLE_KEY_ALIAS);
 
         Mockito.verify(androidLegacyCryptography).deleteKey(SAMPLE_KEY_ALIAS);
-
     }
-
 }
