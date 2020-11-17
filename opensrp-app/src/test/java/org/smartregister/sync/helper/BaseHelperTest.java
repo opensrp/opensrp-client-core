bed5d7974c45aa5a7154dd3a4fa37be16a81f413
Ephraim Kigamba <ekigamba@ona.io>
2020-11-17 15:53:18 +0300

    Mock configuration.drishtiBaseUrl()

--- opensrp-app/src/test/java/org/smartregister/sync/helper/BaseHelperTest.java
+++ opensrp-app/src/test/java/org/smartregister/sync/helper/BaseHelperTest.java
@@ -1,39 +1,25 @@
 package org.smartregister.sync.helper;
 
 import org.junit.Assert;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.robolectric.util.ReflectionHelpers;
-import org.smartregister.AllConstants;
 import org.smartregister.BaseRobolectricUnitTest;
 import org.smartregister.CoreLibrary;
-import org.smartregister.P2POptions;
-import org.smartregister.TestSyncConfiguration;
+import org.smartregister.DristhiConfiguration;
 
 /**
  * Created by Ephraim Kigamba - nek.eam@gmail.com on 10-11-2020.
  */
 public class BaseHelperTest extends BaseRobolectricUnitTest {
-/*
-    @Before
-    public void setUp() throws Exception {
-        ReflectionHelpers.setStaticField(CoreLibrary.class, "instance", null);
-        CoreLibrary.init(context, new TestSyncConfiguration(), 1588062490000l, new P2POptions(true));
-    }*/
-
-
-    @BeforeClass
-    public static void resetCoreLibrary() {
-        ReflectionHelpers.setStaticField(CoreLibrary.class, "instance", null);
-    }
 
     @Test
     public void getFormattedBaseUrl() {
+        String url = "http://site.com/";
 
-        CoreLibrary.getInstance().context().allSharedPreferences()
-                .getPreferences().edit()
-                .putString(AllConstants.DRISHTI_BASE_URL, "http://site.com/").commit();
+        DristhiConfiguration dristhiConfiguration = Mockito.spy(CoreLibrary.getInstance().context().configuration());
+        ReflectionHelpers.setField(CoreLibrary.getInstance().context(), "configuration", dristhiConfiguration);
+        Mockito.doReturn(url).when(dristhiConfiguration).dristhiBaseURL();
 
         Assert.assertNotNull(CoreLibrary.getInstance().context().configuration().dristhiBaseURL());
 
