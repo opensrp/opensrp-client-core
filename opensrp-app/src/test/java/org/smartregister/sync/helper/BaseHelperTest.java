ef73455ef5505cba7bf0de7b4c2cb908da6d56c9
Ephraim Kigamba <ekigamba@ona.io>
2020-11-17 13:56:08 +0300

    Reset CoreLibrary singleton in BaseHelperTest

--- opensrp-app/src/test/java/org/smartregister/sync/helper/BaseHelperTest.java
+++ opensrp-app/src/test/java/org/smartregister/sync/helper/BaseHelperTest.java
@@ -2,16 +2,31 @@ package org.smartregister.sync.helper;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.robolectric.util.ReflectionHelpers;
 import org.smartregister.AllConstants;
 import org.smartregister.BaseRobolectricUnitTest;
 import org.smartregister.CoreLibrary;
+import org.smartregister.P2POptions;
+import org.smartregister.TestSyncConfiguration;
 
 /**
  * Created by Ephraim Kigamba - nek.eam@gmail.com on 10-11-2020.
  */
 public class BaseHelperTest extends BaseRobolectricUnitTest {
+/*
+    @Before
+    public void setUp() throws Exception {
+        ReflectionHelpers.setStaticField(CoreLibrary.class, "instance", null);
+        CoreLibrary.init(context, new TestSyncConfiguration(), 1588062490000l, new P2POptions(true));
+    }*/
+
+
+    @BeforeClass
+    public static void resetCoreLibrary() {
+        ReflectionHelpers.setStaticField(CoreLibrary.class, "instance", null);
+    }
 
     @Test
     public void getFormattedBaseUrl() {
