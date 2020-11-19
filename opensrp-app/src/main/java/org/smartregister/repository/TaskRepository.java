b90e55c8646e1bf4429b7a56c7e39b65b10a1265
Samuel Githengi <samuel.githengi@gmail.com>
2020-11-19 18:38:17 +0300

    replace priority with enum name

--- opensrp-app/src/main/java/org/smartregister/repository/TaskRepository.java
+++ opensrp-app/src/main/java/org/smartregister/repository/TaskRepository.java
@@ -131,7 +131,7 @@ public class TaskRepository extends BaseRepository {
         DatabaseMigrationUtils.addColumnIfNotExists(database, TASK_TABLE, RESTRICTION_START, VARCHAR);
         DatabaseMigrationUtils.addColumnIfNotExists(database, TASK_TABLE, RESTRICTION_END, VARCHAR);
         DatabaseMigrationUtils.recreateSyncTableWithExistingColumnsOnly(database, TASK_TABLE, COLUMNS, CREATE_TASK_TABLE);
-        database.execSQL(String.format("UPDATE %s SET %s=?", TASK_TABLE, PRIORITY), new Object[]{"routine"});
+        database.execSQL(String.format("UPDATE %s SET %s=?", TASK_TABLE, PRIORITY), new Object[]{Task.TaskPriority.ROUTINE.name()});
     }
 
     public void addOrUpdate(Task task) {
