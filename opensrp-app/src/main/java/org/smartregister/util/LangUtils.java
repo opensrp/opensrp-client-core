1ed879246267dfb7ef6ea2db71ca5d03ef222941
Ephraim Kigamba <ekigamba@ona.io>
2020-11-20 20:10:50 +0300

    Fix locale not changing bug due to AndroidX and locale update strategy
    - Update AndroidX Appcompat to 1.2.0
    - Change configuration and locale based on Android version
    
    Fixes #699

--- opensrp-app/src/main/java/org/smartregister/util/LangUtils.java
+++ opensrp-app/src/main/java/org/smartregister/util/LangUtils.java
@@ -3,13 +3,17 @@ package org.smartregister.util;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.os.Build;
+import android.os.LocaleList;
 import android.preference.PreferenceManager;
+
+import androidx.annotation.RequiresApi;
 
 import org.smartregister.repository.AllSharedPreferences;
 
+import java.util.LinkedHashSet;
 import java.util.Locale;
-
-import timber.log.Timber;
+import java.util.Set;
 
 public class LangUtils {
 
@@ -26,18 +30,44 @@ public class LangUtils {
     }
 
     public static Context setAppLocale(Context context, String language) {
+        // This has been copied directly from
+        // https://stackoverflow.com/a/59950631/5588557 & tested to work with android 7
+        return updateResources(context, language);
+    }
+
+    private static Context updateResources(Context context, String language) {
         Locale locale = new Locale(language);
+        Locale.setDefault(locale);
 
         Resources res = context.getResources();
-        Configuration conf = res.getConfiguration();
-        conf.setLocale(locale);
-        try {
-            context = context.createConfigurationContext(conf);
-        } catch (Exception e) {
-            Timber.e(e);
+        Configuration config = new Configuration(res.getConfiguration());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            setLocaleForApi24(config, locale);
+            context = context.createConfigurationContext(config);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            config.setLocale(locale);
+            context = context.createConfigurationContext(config);
+        } else {
+            config.locale = locale;
+            res.updateConfiguration(config, res.getDisplayMetrics());
+        }
+        return context;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private static void setLocaleForApi24(Configuration config, Locale target) {
+        Set<Locale> set = new LinkedHashSet<>();
+        // bring the target locale to the front of the list
+        set.add(target);
+
+        LocaleList all = LocaleList.getDefault();
+        for (int i = 0; i < all.size(); i++) {
+            // append other locales supported by the user
+            set.add(all.get(i));
         }
 
-        return context;
+        Locale[] locales = set.toArray(new Locale[0]);
+        config.setLocales(new LocaleList(locales));
     }
 
 }
