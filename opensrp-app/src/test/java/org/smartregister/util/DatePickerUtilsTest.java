ec8af311b8b7ea34ec1670d6ceb3769944e374ea
Ephraim Kigamba <ekigamba@ona.io>
2020-11-24 12:45:25 +0300

    Remove buggy dialog date picker theming method

--- opensrp-app/src/test/java/org/smartregister/util/DatePickerUtilsTest.java
+++ opensrp-app/src/test/java/org/smartregister/util/DatePickerUtilsTest.java
@@ -19,6 +19,7 @@ import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.robolectric.Robolectric;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.shadows.ShadowDatePickerDialog;
 import org.smartregister.BaseRobolectricUnitTest;
@@ -28,54 +29,6 @@ import org.smartregister.R;
  * Created by Ephraim Kigamba - nek.eam@gmail.com on 29-09-2020.
  */
 public class DatePickerUtilsTest extends BaseRobolectricUnitTest {
-
-    @Ignore
-    @Test
-    public void testThemeDatePickerShouldAddNumberPickersInDialogWhenGivenDatePickerDialog() {
-        /*TestDialogActivity testDialogActivity = Robolectric.buildActivity(TestDialogActivity.class)
-                .create()
-                .start()
-                .resume()
-                .visible()
-                .get();
-        DatePickerDialog datePickerDialog = testDialogActivity.getDatePickerDialog();*/
-
-        /*DatePickerDialog datePickerDialog = new DatePickerDialog(RuntimeEnvironment.application);
-        datePickerDialog.show();*/
-
-        DatePickerDialog datePickerDialog = (DatePickerDialog) ShadowDatePickerDialog.getLatestDialog();
-        datePickerDialog.updateDate(2010, 5, 10);
-        DatePicker datePicker = datePickerDialog.getDatePicker();
-
-
-        int idYear = Resources.getSystem().getIdentifier("year", "id", "android");
-        int idMonth = Resources.getSystem().getIdentifier("month", "id", "android");
-        int idDay = Resources.getSystem().getIdentifier("day", "id", "android");
-
-        int idLayout = Resources.getSystem().getIdentifier("pickers", "id", "android");
-        LinearLayout layout = datePicker.findViewById(idLayout);
-
-        // Call the method under test
-        DatePickerUtils.themeDatePicker(datePickerDialog, new char[]{'m', 'y', 'd'});
-
-        // Assert the properties
-        Assert.assertEquals(3, layout.getChildCount());
-
-        int idPickerInput = Resources.getSystem().getIdentifier("numberpicker_input", "id", "android");
-        TextView dayDateTv = ((NumberPicker) layout.getChildAt(0)).findViewById(idPickerInput);
-        Assert.assertEquals(EditorInfo.IME_ACTION_NEXT, dayDateTv.getImeOptions());
-
-        TextView monthDateTv = ((NumberPicker) layout.getChildAt(1)).findViewById(idPickerInput);
-        Assert.assertEquals(EditorInfo.IME_ACTION_NEXT, monthDateTv.getImeOptions());
-
-        TextView yearDateTv = ((NumberPicker) layout.getChildAt(2)).findViewById(idPickerInput);
-        Assert.assertEquals(EditorInfo.IME_ACTION_DONE, yearDateTv.getImeOptions());
-
-        // Assert the order of views
-        Assert.assertEquals(idMonth, layout.getChildAt(0).getId());
-        Assert.assertEquals(idYear, layout.getChildAt(1).getId());
-        Assert.assertEquals(idDay, layout.getChildAt(2).getId());
-    }
 
     @Test
     public void preventShowingKeyboard() {
@@ -141,6 +94,7 @@ public class DatePickerUtilsTest extends BaseRobolectricUnitTest {
             setContentView(R.layout.html);
 
             datePickerDialog = new DatePickerDialog(TestDialogActivity.this);
+            datePickerDialog.updateDate(1, 1, 1);
             datePickerDialog.show();
         }
 
