eb8bdf77964b948cc557b6f2114556fea26f68b4
Allan O <a.onchuru@gmail.com>
2020-11-24 19:17:35 +0300

    :construction: Implement show profile details in Activity

--- opensrp-app/src/main/java/org/smartregister/view/activity/BaseConfigurableMemberProfileActivity.java
+++ opensrp-app/src/main/java/org/smartregister/view/activity/BaseConfigurableMemberProfileActivity.java
@@ -1,6 +1,10 @@
 package org.smartregister.view.activity;
 
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
@@ -9,29 +13,33 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
-import com.google.android.material.appbar.CollapsingToolbarLayout;
-
+import org.joda.time.DateTime;
+import org.joda.time.Days;
 import org.json.JSONObject;
 import org.smartregister.AllConstants;
 import org.smartregister.CoreLibrary;
 import org.smartregister.R;
 import org.smartregister.commonregistry.CommonPersonObjectClient;
 import org.smartregister.configuration.ModuleConfiguration;
+import org.smartregister.domain.AlertStatus;
 import org.smartregister.helper.ImageRenderHelper;
 import org.smartregister.util.Utils;
-import org.smartregister.view.contract.BaseProfileContract;
 import org.smartregister.view.contract.ConfigurableMemberProfileActivityContract;
 import org.smartregister.view.presenter.BaseConfigurableMemberProfilePresenter;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class BaseConfigurableMemberProfileActivity extends BaseProfileActivity implements BaseProfileContract.View {
+public class BaseConfigurableMemberProfileActivity extends BaseProfileActivity implements ConfigurableMemberProfileActivityContract.View {
 
     protected String moduleName;
     protected ModuleConfiguration moduleConfiguration;
     protected CommonPersonObjectClient client;
-    private CollapsingToolbarLayout collapsingToolbarLayout;
     protected CircleImageView profileImageView;
+    private TextView tvTitle;
     private TextView tvNameAndAge;
     private TextView tvPrimaryCareGiver;
     private TextView tvGender;
@@ -64,36 +72,43 @@ public class BaseConfigurableMemberProfileActivity extends BaseProfileActivity i
         return moduleName;
     }
 
+
     @Override
     protected void onCreation() {
         setContentView(R.layout.activity_base_configurable_member_profile);
 
         Toolbar toolbar = findViewById(R.id.collapsing_toolbar);
+        tvTitle = toolbar.findViewById(R.id.toolbar_title);
         setSupportActionBar(toolbar);
 
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
+            final Drawable upArrow = getResources().getDrawable(R.drawable.ic_action_goldsmith_menu_arrow);
+            upArrow.setColorFilter(getResources().getColor(R.color.text_blue), PorterDuff.Mode.SRC_ATOP);
+            actionBar.setHomeAsUpIndicator(upArrow);
         }
+        toolbar.setNavigationOnClickListener(v -> onBackPressed());
 
         appBarLayout = findViewById(R.id.collapsing_toolbar_appbarlayout);
-        // Set collapsing tool bar title.
-        collapsingToolbarLayout = appBarLayout.findViewById(R.id.collapsing_toolbar_layout);
-        appBarLayout.addOnOffsetChangedListener(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            appBarLayout.setOutlineProvider(null);
+        }
         imageRenderHelper = new ImageRenderHelper(this);
 
-        initializePresenter();
         setupViews();
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             client = (CommonPersonObjectClient) getIntent().getSerializableExtra(AllConstants.INTENT_KEY.COMMON_PERSON_CLIENT);
         }
+
+        initializePresenter();
+        presenter().fetchProfileData(client);
     }
 
     @Override
     protected void setupViews() {
-        super.setupViews();
         profileImageView = findViewById(R.id.profile_imageview);
         tvNameAndAge = findViewById(R.id.textview_name_age);
         tvPrimaryCareGiver = findViewById(R.id.textview_primary_caregiver);
@@ -128,8 +143,8 @@ public class BaseConfigurableMemberProfileActivity extends BaseProfileActivity i
     }
 
     @Override
-    protected BaseConfigurableMemberProfilePresenter initializePresenter() {
-        return new BaseConfigurableMemberProfilePresenter(getModuleConfiguration(), this);
+    protected void initializePresenter() {
+        presenter = new BaseConfigurableMemberProfilePresenter(getModuleConfiguration(), this);
     }
 
     @Override
@@ -139,7 +154,7 @@ public class BaseConfigurableMemberProfileActivity extends BaseProfileActivity i
 
     @Override
     protected void fetchProfileData() {
-
+        presenter().fetchProfileData(client);
     }
 
     @Override
@@ -147,7 +162,7 @@ public class BaseConfigurableMemberProfileActivity extends BaseProfileActivity i
         if (presenter == null) {
             initializePresenter();
         }
-        return presenter;
+        return (ConfigurableMemberProfileActivityContract.Presenter) presenter;
     }
 
     @Override
@@ -183,21 +198,61 @@ public class BaseConfigurableMemberProfileActivity extends BaseProfileActivity i
 
     @Override
     public void startFormActivity(JSONObject formJson) {
+        // TODO
+    }
 
+    @Override
+    public void setLastVisit(Date lastVisitDate) {
+        if (lastVisitDate == null)
+            return;
+
+        if (lastVisitDate != null) {
+            medicalHistoryRow.setVisibility(View.VISIBLE);
+            int numOfDays = Days.daysBetween(new DateTime(lastVisitDate).toLocalDate(), new DateTime().toLocalDate()).getDays();
+            tvLastVisitDaysAgo.setText(getString(R.string.last_visit_days_ago, (numOfDays <= 1) ? getString(R.string.less_than_twenty_four) : String.valueOf(numOfDays) + " " + getString(R.string.days)));
+        }
+    }
+
+    @Override
+    public void setUpComingServicesStatus(String service, AlertStatus status, Date date) {
+        if (status == AlertStatus.complete)
+            return;
+
+        upcomingServicesRow.setVisibility(View.VISIBLE);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM", Locale.getDefault());
+        if (status == AlertStatus.upcoming) {
+            tvUpcomingServicesDue.setText(Html.fromHtml(getString(R.string.service_upcoming, service, dateFormat.format(date))));
+        } else if (status == AlertStatus.urgent) {
+            tvUpcomingServicesDue.setText(Html.fromHtml(getString(R.string.service_overdue, service, dateFormat.format(date))));
+        } else {
+            tvUpcomingServicesDue.setText(Html.fromHtml(getString(R.string.service_due, service, dateFormat.format(date))));
+        }
+    }
+
+    @Override
+    public void setFamilyStatus(AlertStatus status) {
+        familyProfileRow.setVisibility(View.VISIBLE);
+        if (status == AlertStatus.complete) {
+            tvFamilyDueServices.setText(getString(R.string.family_has_nothing_due));
+        } else if (status == AlertStatus.normal) {
+            tvFamilyDueServices.setText(getString(R.string.family_has_services_due));
+        } else if (status == AlertStatus.urgent) {
+            tvFamilyDueServices.setText(Html.fromHtml(getString(R.string.family_has_service_overdue)));
+        }
     }
 
     @Override
     public void openMedicalHistory() {
-
+        // TODO
     }
 
     @Override
     public void openUpcomingServices() {
-
+        // TODO
     }
 
     @Override
     public void openFamilyDueServices() {
-
+        // TODO
     }
 }
