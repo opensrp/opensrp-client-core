fb45243751f90e05ca6ac3a3c0b320f58b749eee
Samuel Githengi <samuel.githengi@gmail.com>
2020-12-03 10:53:56 +0300

    update unit test

--- opensrp-app/src/main/java/org/smartregister/repository/TaskRepository.java
+++ opensrp-app/src/main/java/org/smartregister/repository/TaskRepository.java
@@ -381,7 +381,7 @@ public class TaskRepository extends BaseRepository {
     }
 
     public List<Task> getAllUnsynchedCreatedTasks() {
-        return new ArrayList<>(getTasks(String.format("SELECT *  FROM %s WHERE %s =? OR %s IS NULL OR %s = 0 ", TASK_TABLE, SYNC_STATUS, SERVER_VERSION,SERVER_VERSION), new String[]{BaseRepository.TYPE_Created}));
+        return new ArrayList<>(getTasks(String.format("SELECT *  FROM %s WHERE %s =? OR %s IS NULL OR %s = 0", TASK_TABLE, SYNC_STATUS, SERVER_VERSION,SERVER_VERSION), new String[]{BaseRepository.TYPE_Created}));
     }
 
     /**
