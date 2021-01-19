e0fde6539c83bad22167f1ec1050760faadc33bf
Ephraim Kigamba <ekigamba@ona.io>
2021-01-19 10:43:49 +0300

    Add tests for Context
    - Add test for Context#getInstance
    - Add test for Context#setInstance

--- opensrp-app/src/test/java/org/smartregister/ContextRobolectricTest.java
+++ opensrp-app/src/test/java/org/smartregister/ContextRobolectricTest.java
@@ -1,9 +1,27 @@
 package org.smartregister;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.robolectric.util.ReflectionHelpers;
+
 /**
  * Created by Ephraim Kigamba - nek.eam@gmail.com on 19-01-2021.
  */
 public class ContextRobolectricTest extends BaseRobolectricUnitTest {
 
+    @Test
+    public void getInstanceShouldCallConstructorWhenContextIsNull() {
+        ReflectionHelpers.setStaticField(Context.class, "context", null);
+        Assert.assertNull(ReflectionHelpers.getStaticField(Context.class, "context"));
+
+        Context.getInstance();
+
+        Assert.assertNotNull(ReflectionHelpers.getStaticField(Context.class, "context"));
+    }
+
+    @Test
+    public void setInstanceShouldReturnNullWhenContextPassedIsNull() {
+        Assert.assertNull(Context.setInstance(null));
+    }
 
 }
