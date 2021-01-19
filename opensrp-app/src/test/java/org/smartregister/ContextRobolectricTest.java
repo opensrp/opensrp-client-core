c2fcc2ea3c3b2a99d6b8fa723cf415b6e9191997
Ephraim Kigamba <ekigamba@ona.io>
2021-01-19 11:21:49 +0300

    Add tests for Context#applicationContext

--- opensrp-app/src/test/java/org/smartregister/ContextRobolectricTest.java
+++ opensrp-app/src/test/java/org/smartregister/ContextRobolectricTest.java
@@ -1,7 +1,12 @@
 package org.smartregister;
 
+import android.content.res.Configuration;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.robolectric.util.ReflectionHelpers;
 
 /**
@@ -22,6 +27,40 @@ public class ContextRobolectricTest extends BaseRobolectricUnitTest {
     @Test
     public void setInstanceShouldReturnNullWhenContextPassedIsNull() {
         Assert.assertNull(Context.setInstance(null));
+    }
+
+    @Test
+    public void applicationContextShouldReturnNullWhenApplicationContextIsNull() {
+        android.content.Context context = ReflectionHelpers.getField(Context.getInstance(), "applicationContext");
+        Context.getInstance().setApplicationContext(null);
+
+        // Call the actual method
+        Assert.assertNull(Context.getInstance().applicationContext());
+
+        // Return the previous context
+        Context.getInstance().setApplicationContext(context);
+    }
+
+    @Test
+    public void applicationContextShouldReturnThrowExceptionAndReturnApplicationContext() {
+        android.content.Context context = ReflectionHelpers.getField(Context.getInstance(), "applicationContext");
+        android.content.Context spiedContext = Mockito.spy(context);
+        Context.getInstance().setApplicationContext(spiedContext);
+
+        Mockito.doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                throw new Exception("An exception");
+            }
+        }).when(spiedContext).getResources();
+
+        // Call the actual method
+        Assert.assertEquals(spiedContext, Context.getInstance().applicationContext());
+
+        Mockito.verify(spiedContext, Mockito.times(0)).createConfigurationContext(Mockito.any(Configuration.class));
+
+        // Return the previous context
+        Context.getInstance().setApplicationContext(context);
     }
 
 }
