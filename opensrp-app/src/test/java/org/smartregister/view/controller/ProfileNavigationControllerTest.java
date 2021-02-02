50c32240b48b95d83e696f800639587654d1e7c5
Vincent Karuri <vincentmkaruri@gmail.com>
2021-02-02 10:51:03 +0300

    Add ProfileNavigationController tests

---
+++ opensrp-app/src/test/java/org/smartregister/view/controller/ProfileNavigationControllerTest.java
@@ -0,0 +1,39 @@
+package org.smartregister.view.controller;
+
+import android.content.Intent;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.robolectric.RuntimeEnvironment;
+import org.smartregister.BaseUnitTest;
+import org.smartregister.view.activity.EligibleCoupleDetailActivity;
+
+import static org.junit.Assert.*;
+import static org.robolectric.Shadows.shadowOf;
+import static org.smartregister.AllConstants.CASE_ID;
+
+/**
+ * Created by Vincent Karuri on 02/02/2021
+ */
+public class ProfileNavigationControllerTest extends BaseUnitTest {
+
+    private ProfileNavigationController profileNavigationController;
+
+    @Before
+    public void setUp() throws Exception {
+        profileNavigationController = new ProfileNavigationController();
+    }
+
+    @Test
+    public void testNavigateToECProfileShouldNavigateToProfile() {
+        final String CASE_ID = "case_id";
+
+        Intent expectedIntent = new Intent(RuntimeEnvironment.application,
+                EligibleCoupleDetailActivity.class);
+        expectedIntent.putExtra(CASE_ID, CASE_ID);
+
+        profileNavigationController.navigateToECProfile(RuntimeEnvironment.application, CASE_ID);
+        Intent actualIntent = shadowOf(RuntimeEnvironment.application).getNextStartedActivity();
+        assertEquals(expectedIntent.getComponent(), actualIntent.getComponent());
+    }
+}
