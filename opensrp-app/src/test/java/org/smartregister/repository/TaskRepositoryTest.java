96c3ab2efb72b6c4721d4fe43887f8563b3e1620
richard <rkareko@ona.io>
2021-02-10 15:49:33 +0300

    Add task repository tests

--- opensrp-app/src/test/java/org/smartregister/repository/TaskRepositoryTest.java
+++ opensrp-app/src/test/java/org/smartregister/repository/TaskRepositoryTest.java
@@ -536,4 +536,80 @@ public class TaskRepositoryTest extends BaseUnitTest {
 
     }
 
+    @Test
+    public void testGetEntityIdsWithDuplicateTasks() {
+
+        String entityId = "entity-id-1";
+        MatrixCursor cursor = new MatrixCursor(new String[]{"for"});
+        cursor.addRow(new Object[]{entityId});
+        String query = "SELECT " +
+                "    DISTINCT(for) " +
+                "FROM " +
+                "    task " +
+                "GROUP BY " +
+                "    plan_id, for, code " +
+                "HAVING " +
+                "    COUNT(*) > 1";
+
+        when(sqLiteDatabase.rawQuery(query, new String[]{})).thenReturn(cursor);
+
+        List<String> entities = taskRepository.getEntityIdsWithDuplicateTasks();
+        assertEquals(1, entities.size());
+        assertEquals(entityId, entities.get(0));
+
+    }
+
+    @Test
+    public void testGetDuplicateTasksForEntity() {
+        MatrixCursor cursor = getCursor();
+        Task task = gson.fromJson(taskJson, Task.class);
+        task.setIdentifier("task-2-identifier");
+        String entityId = "structure-id-1";
+        List<String> taskIds = new ArrayList<>();
+        taskIds.add("tsk11231jh22");
+        taskIds.add("task-2-identifier");
+        cursor.addRow(new Object[]{2, task.getIdentifier(), task.getPlanIdentifier(), task.getGroupIdentifier(),
+                task.getStatus().name(), task.getBusinessStatus(), task.getPriority().name(), task.getCode(),
+                task.getDescription(), task.getFocus(), task.getForEntity(),
+                task.getExecutionPeriod().getStart().getMillis(),
+                null,
+                task.getAuthoredOn().getMillis(), task.getLastModified().getMillis(),
+                task.getOwner(), task.getSyncStatus(), task.getServerVersion(), task.getStructureId(), task.getReasonReference(), null, null, null, null, null});
+
+        String query = "SELECT t1.* " +
+                "FROM task t1 " +
+                "JOIN (SELECT " +
+                "    _id, plan_id, for, code, COUNT(*) as count " +
+                "FROM " +
+                "    task " +
+                "GROUP BY " +
+                "    plan_id, for, code " +
+                "HAVING  " +
+                "    COUNT(*) > 1) t2 " +
+                "ON t1.plan_id = t2.plan_id " +
+                "AND t1.for = t2.for " +
+                "AND t1.code = t2.code " +
+                "AND t1.for = ? " +
+                "ORDER BY t1.for";
+        when(sqLiteDatabase.rawQuery(query, new String[]{entityId})).thenReturn(cursor);
+
+        Set<Task> duplicateTasks = taskRepository.getDuplicateTasksForEntity(entityId);
+        assertEquals(2, duplicateTasks.size());
+        for (Task taskEntity : duplicateTasks) {
+                assertTrue(taskIds.contains(taskEntity.getIdentifier()));
+        }
+    }
+
+    @Test
+    public void testDeleteTasksByIds(){
+        List<String> taskIds = new ArrayList<>();
+        taskIds.add("taskId-1");
+        taskIds.add("taskId-2");
+        taskRepository.deleteTasksByIds(taskIds);
+        verify(sqLiteDatabase).delete(stringArgumentCaptor.capture(), stringArgumentCaptor.capture(), argsCaptor.capture());
+        assertEquals("task", stringArgumentCaptor.getAllValues().get(0));
+        assertEquals("_id in ('taskId-1', 'taskId-2')", stringArgumentCaptor.getAllValues().get(1));
+        assertNull(argsCaptor.getValue());
+    }
+
 }
