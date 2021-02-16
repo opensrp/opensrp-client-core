b63156c47d1f2fe04d506a4ce2fc53f551501f93
Ephraim Kigamba <ekigamba@ona.io>
2021-02-16 10:34:47 +0300

    Fix ignored test SyncUtils#logoutUser

--- opensrp-app/src/test/java/org/smartregister/util/SyncUtilsRobolectricTest.java
+++ opensrp-app/src/test/java/org/smartregister/util/SyncUtilsRobolectricTest.java
@@ -1,8 +1,12 @@
 package org.smartregister.util;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.accounts.AccountManagerFuture;
 import android.accounts.AuthenticatorException;
 import android.accounts.OperationCanceledException;
 import android.content.Intent;
+import android.os.Bundle;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -16,6 +20,7 @@ import org.smartregister.BaseRobolectricUnitTest;
 import org.smartregister.Context;
 import org.smartregister.CoreLibrary;
 import org.smartregister.R;
+import org.smartregister.account.AccountHelper;
 import org.smartregister.service.UserService;
 
 import java.io.IOException;
@@ -42,12 +47,31 @@ public class SyncUtilsRobolectricTest extends BaseRobolectricUnitTest {
         ReflectionHelpers.setField(syncUtils, "opensrpContext", opensrpContext);
     }
 
-    @Ignore
     @Test
     public void logoutUser() throws AuthenticatorException, OperationCanceledException, IOException {
         ArgumentCaptor<Intent> intentArgumentCaptor = ArgumentCaptor.forClass(Intent.class);
         Mockito.doNothing().when(context).startActivity(intentArgumentCaptor.capture());
 
+        // Mock AccountManager
+        AccountManager originalAccountManager =  ReflectionHelpers.getStaticField(AccountHelper.class, "accountManager");
+        AccountManager accountManager =  Mockito.mock(AccountManager.class);
+        ReflectionHelpers.setStaticField(AccountHelper.class, "accountManager", accountManager);
+
+        final String CORE_ACCOUNT_NAME = "demo";
+        String CORE_ACCOUNT_TYPE = "org.smartregister.core";
+        Account[] accounts = {new Account(CORE_ACCOUNT_NAME, CORE_ACCOUNT_TYPE)};
+        Mockito.doReturn(accounts).when(accountManager).getAccountsByType(CORE_ACCOUNT_TYPE);
+        AccountManagerFuture<Bundle> accountManagerFuture = Mockito.mock(AccountManagerFuture.class);
+        Intent accountAuthenticatorIntent = new Intent();
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(AccountManager.KEY_INTENT, accountAuthenticatorIntent);
+        Mockito.doReturn(bundle).when(accountManagerFuture).getResult();
+        Mockito.doReturn(accountManagerFuture).when(accountManager).updateCredentials(accounts[0], "provider", null, null, null, null);
+
+        // Mock the registered anm
+        CoreLibrary.getInstance().context().allSharedPreferences().updateANMUserName("demo");
+
+        // Call the method under test
         syncUtils.logoutUser(R.string.logout_text);
 
         Mockito.verify(userService).forceRemoteLogin(Mockito.anyString());
@@ -56,14 +80,16 @@ public class SyncUtilsRobolectricTest extends BaseRobolectricUnitTest {
         Intent intent = intentArgumentCaptor.getValue();
         Assert.assertEquals(Intent.ACTION_MAIN, intent.getAction());
         Assert.assertTrue(intent.getCategories().contains(Intent.CATEGORY_LAUNCHER));
-        Assert.assertEquals("org.smartregister", intent.getPackage());
+        Assert.assertEquals("org.smartregister.test", intent.getPackage());
 
         Mockito.verify(context).startActivity(Mockito.any(Intent.class));
 
+        // Return the original account manager
+        ReflectionHelpers.setStaticField(AccountHelper.class, "accountManager", originalAccountManager);
     }
 
     @Test
-    public void getLogoutUserIntentShouldProvideAnonymouseIntentWhenLauncherActivityIsUnavailable() {
+    public void getLogoutUserIntentShouldProvideAnonymousIntentWhenLauncherActivityIsUnavailable() {
         ArgumentCaptor<Intent> intentArgumentCaptor = ArgumentCaptor.forClass(Intent.class);
         Mockito.doNothing().when(context).startActivity(intentArgumentCaptor.capture());
 
