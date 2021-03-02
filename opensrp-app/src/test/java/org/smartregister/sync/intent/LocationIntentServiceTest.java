15e2dcf0e5de05a0816fe8174dd994f9357edb52
Vincent Karuri <vincentmkaruri@gmail.com>
2021-03-02 10:59:17 +0300

    Add LocationIntentService tests

---
+++ opensrp-app/src/test/java/org/smartregister/sync/intent/LocationIntentServiceTest.java
@@ -0,0 +1,43 @@
+package org.smartregister.sync.intent;
+
+import android.content.Intent;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.powermock.reflect.Whitebox;
+import org.robolectric.annotation.Config;
+import org.smartregister.BaseUnitTest;
+import org.smartregister.shadows.ShadowContextCompat;
+import org.smartregister.shadows.ShadowLocationServiceHelper;
+import org.smartregister.sync.helper.LocationServiceHelper;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Vincent Karuri on 02/03/2021
+ */
+
+@Config(shadows = {ShadowLocationServiceHelper.class})
+public class LocationIntentServiceTest extends BaseUnitTest {
+
+    @Mock
+    private LocationServiceHelper locationServiceHelper;
+
+    private LocationIntentService locationIntentService;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        locationIntentService = new LocationIntentService();
+    }
+
+    @Test
+    public void testOnHandleIntentShouldFetchLocations() throws Exception {
+        ShadowLocationServiceHelper.setInstance(locationServiceHelper);
+        Whitebox.invokeMethod(locationIntentService, "onHandleIntent", null);
+        Mockito.verify(locationServiceHelper).fetchLocationsStructures();
+    }
+}
