df3d7380b9317e0ff361ff575869a6523fa41075
faiq <mfaiq1996@gmail.com>
2021-03-22 17:35:04 +0500

    validate auth client exists for new url when changing url from settings

--- opensrp-app/src/main/java/org/smartregister/util/UrlUtil.java
+++ opensrp-app/src/main/java/org/smartregister/util/UrlUtil.java
@@ -1,9 +1,15 @@
 package org.smartregister.util;
 
 import org.apache.commons.validator.routines.UrlValidator;
+import org.smartregister.EnvironmentManager;
 
 public class UrlUtil {
     public static boolean isValidUrl(String s){
         return new UrlValidator(new String[]{"http", "https"}).isValid(s);
+    }
+
+    public static boolean isValidEnvironment(String url) {
+        EnvironmentManager manager = EnvironmentManager.getInstance();
+        return manager.getEnvironments().isEmpty() || manager.getEnvironment(url) != null;
     }
 }
