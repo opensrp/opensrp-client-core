7eb985be3b4a3c15c98d3d92eab7bad4b27196a3
Vincent Karuri <vincentmkaruri@gmail.com>
2021-06-08 11:19:14 +0300

    Add SyncAllLocationsIntentService test

---
+++ opensrp-app/src/test/java/org/smartregister/sync/intent/SyncAllLocationsIntentServiceTest.java
@@ -0,0 +1,33 @@
+package org.smartregister.sync.intent;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.powermock.reflect.Whitebox;
+import org.robolectric.util.ReflectionHelpers;
+import org.smartregister.BaseUnitTest;
+import org.smartregister.sync.helper.LocationServiceHelper;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Vincent Karuri on 08/06/2021
+ */
+public class SyncAllLocationsIntentServiceTest extends BaseUnitTest {
+
+    private SyncAllLocationsIntentService syncAllLocationsIntentService;
+
+    @Before
+    public void setUp() throws Exception {
+        syncAllLocationsIntentService = new SyncAllLocationsIntentService();
+    }
+
+    @Test
+    public void onHandleIntent() {
+        LocationServiceHelper locationServiceHelper = Mockito.mock(LocationServiceHelper.class);
+        Whitebox.setInternalState(LocationServiceHelper.class, "instance", locationServiceHelper);
+        syncAllLocationsIntentService.onHandleIntent(null);
+        Mockito.verify(locationServiceHelper).fetchAllLocations();
+        Whitebox.setInternalState(LocationServiceHelper.class, "instance", (LocationServiceHelper) null);
+    }
+}
