7e5265f53bd39cd9844354f6fefa08a57516ec43
qaziabubakar-vd <qaziabubakar47@gmail.com>
2021-10-04 12:56:31 +0500

    - Removed unused import

--- opensrp-app/src/test/java/org/smartregister/view/fragment/StatsFragmentTest.java
+++ opensrp-app/src/test/java/org/smartregister/view/fragment/StatsFragmentTest.java
@@ -14,7 +14,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.fragment.app.FragmentManager;
