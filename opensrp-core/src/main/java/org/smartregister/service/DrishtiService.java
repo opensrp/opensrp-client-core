41762c9c94b5e9cbb96487a617930c65d1830f38
Simon Njoroge <snjoroge@ona.io>
2022-04-27 12:27:03 +0300

    Allow actions fetch via POST

--- opensrp-core/src/main/java/org/smartregister/service/DrishtiService.java
+++ opensrp-core/src/main/java/org/smartregister/service/DrishtiService.java
@@ -1,36 +1,63 @@
 package org.smartregister.service;
 
+import static org.smartregister.domain.ResponseStatus.failure;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 
+import org.apache.commons.codec.CharEncoding;
 import org.ei.drishti.dto.Action;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.smartregister.domain.Response;
 
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Type;
 import java.net.URLEncoder;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.smartregister.domain.ResponseStatus.failure;
+import timber.log.Timber;
 
 public class DrishtiService {
     private HTTPAgent agent = null;
     private String drishtiBaseURL;
+    private final String ACTIONS_URI = "/actions";
+    private final String ANM_IDENTIFIER = "anmIdentifier";
+    private final String TIMESTAMP = "timeStamp";
 
     public DrishtiService(HTTPAgent agent, String drishtiBaseURL) {
         this.agent = agent;
         this.drishtiBaseURL = drishtiBaseURL;
     }
 
-    public Response<List<Action>> fetchNewActions(String anmIdentifier, String
-            previouslyFetchedIndex) {
-        String anmID = URLEncoder.encode(anmIdentifier);
-        Response<String> response = agent
-                .fetch(drishtiBaseURL + "/actions?anmIdentifier=" + anmID + "&timeStamp="
-                        + previouslyFetchedIndex);
+    public Response<List<Action>> fetchNewActions(String anmIdentifier, String previouslyFetchedIndex) {
+        String anmID = "";
+        try {
+            anmID = URLEncoder.encode(anmIdentifier, CharEncoding.UTF_8);
+        } catch (UnsupportedEncodingException e) {
+            Timber.e(e);
+        }
+
+        JSONObject requestBody = null;
+        try {
+            requestBody = new JSONObject();
+            requestBody.put(ANM_IDENTIFIER, anmID);
+            requestBody.put(TIMESTAMP, previouslyFetchedIndex);
+        } catch (JSONException e) {
+            Timber.e(e);
+        }
+
+        Response<String> response = agent.post(
+                MessageFormat.format("{0}{1}", drishtiBaseURL, ACTIONS_URI),
+                requestBody.toString()
+        );
+
         Type collectionType = new TypeToken<List<Action>>() {
         }.getType();
+
         List<Action> actions;
         try {
             actions = new Gson().fromJson(response.payload(), collectionType);
@@ -40,7 +67,9 @@ public class DrishtiService {
             return new Response<List<Action>>(failure, new ArrayList<Action>());
         }
 
-        return new Response<List<Action>>(response.status(),
-                actions == null ? new ArrayList<Action>() : actions);
+        return new Response<List<Action>>(
+                response.status(),
+                actions == null ? new ArrayList<Action>() : actions
+        );
     }
 }
