1870c6d8a28dc37f8cf7d069051c2d920479325c
Ephraim Kigamba <ekigamba@ona.io>
2022-10-25 12:42:58 +0300

    Run DuplicateCleanerWorker only until all IDs are cleaned

--- opensrp-core/src/main/java/org/smartregister/job/DuplicateCleanerWorker.java
+++ opensrp-core/src/main/java/org/smartregister/job/DuplicateCleanerWorker.java
@@ -3,30 +3,60 @@ package org.smartregister.job;
 import android.content.Context;
 
 import androidx.annotation.NonNull;
+import androidx.work.ExistingPeriodicWorkPolicy;
+import androidx.work.PeriodicWorkRequest;
 import androidx.work.WorkManager;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
+import org.smartregister.AllConstants;
+import org.smartregister.CoreLibrary;
 import org.smartregister.domain.DuplicateZeirIdStatus;
+import org.smartregister.repository.AllSharedPreferences;
 import org.smartregister.util.AppHealthUtils;
+
+import java.util.concurrent.TimeUnit;
 
 import timber.log.Timber;
 
 public class DuplicateCleanerWorker extends Worker {
     private Context mContext;
 
+    public static final String TAG = "DuplicateCleanerWorker";
+
     public DuplicateCleanerWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
         mContext = context;
     }
 
+    public static boolean shouldSchedule() {
+        AllSharedPreferences allSharedPreferences = CoreLibrary.getInstance().context().allSharedPreferences();
+        return !allSharedPreferences.getBooleanPreference(AllConstants.PREF_KEY.DUPLICATE_IDS_FIXED);
+    }
+
+    public static void schedulePeriodically(Context context, int mins) {
+        PeriodicWorkRequest periodicWorkRequest = new PeriodicWorkRequest.Builder(DuplicateCleanerWorker.class, mins, TimeUnit.MINUTES)
+                .build();
+
+        WorkManager.getInstance(context)
+                .enqueueUniquePeriodicWork(TAG, ExistingPeriodicWorkPolicy.KEEP, periodicWorkRequest);
+    }
+
     @NonNull
     @Override
     public Result doWork() {
-        DuplicateZeirIdStatus duplicateZeirIdStatus = AppHealthUtils.cleanUniqueZeirIds();
-        Timber.i("Doing some cleaning work");
-        if (duplicateZeirIdStatus != null && duplicateZeirIdStatus.equals(DuplicateZeirIdStatus.CLEANED))
+        AllSharedPreferences allSharedPreferences = CoreLibrary.getInstance().context().allSharedPreferences();
+
+        if (!allSharedPreferences.getBooleanPreference(AllConstants.PREF_KEY.DUPLICATE_IDS_FIXED)) {
+            DuplicateZeirIdStatus duplicateZeirIdStatus = AppHealthUtils.cleanUniqueZeirIds();
+            Timber.i("Doing some cleaning work");
+            if (duplicateZeirIdStatus != null && duplicateZeirIdStatus.equals(DuplicateZeirIdStatus.CLEANED)) {
+                allSharedPreferences.saveBooleanPreference(AllConstants.PREF_KEY.DUPLICATE_IDS_FIXED, true);
+                WorkManager.getInstance(mContext).cancelWorkById(this.getId());
+            }
+        } else {
             WorkManager.getInstance(mContext).cancelWorkById(this.getId());
+        }
 
         return Result.success();
     }
