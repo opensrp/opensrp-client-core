f224284cb12100bbab6930cbc82eda2ae05480fb
Ephraim Kigamba <ekigamba@ona.io>
2023-03-09 12:46:36 +0300

    Download android 12 robolectric dependency jar

--- download-robolectric-deps.sh
+++ download-robolectric-deps.sh
@@ -9,6 +9,7 @@ wget -nc https://repo1.maven.org/maven2/org/robolectric/android-all/7.0.0_r1-rob
 wget -nc https://repo1.maven.org/maven2/org/robolectric/android-all/7.1.0_r7-robolectric-r1/android-all-7.1.0_r7-robolectric-r1.jar -P robolectric-deps
 wget -nc https://repo1.maven.org/maven2/org/robolectric/android-all/9-robolectric-4913185-2/android-all-9-robolectric-4913185-2.jar -P robolectric-deps
 wget -nc https://repo1.maven.org/maven2/org/robolectric/android-all/11-robolectric-6757853/android-all-11-robolectric-6757853.jar -P robolectric-deps
+wget -nc https://repo1.maven.org/maven2/org/robolectric/android-all-instrumented/12-robolectric-7732740-i4/android-all-instrumented-12-robolectric-7732740-i4.jar -P robolectric-deps
 wget -nc https://repo1.maven.org/maven2/org/robolectric/android-all/5.1.1_r9-robolectric-r2/android-all-5.1.1_r9-robolectric-r2.jar -P robolectric-deps
 
 cp robolectric-deps.properties opensrp-core/src/test/resources
